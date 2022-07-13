cfec41b08b61d968e1d6034adcdb1e45cfcb3ff4
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-13 23:56:37 +0530

    change board and boardcard

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AppImage: typeof import('./src/components/AppImage.vue')['default']
     Board: typeof import('./src/components/Board.vue')['default']
+    BoardCard: typeof import('./src/components/BoardCard.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheDrawer: typeof import('./src/components/TheDrawer.vue')['default']
--- src/pages/boards/index.vue
+++ src/pages/boards/index.vue
@@ -1,65 +1,44 @@
-<template>
-  <div class="px-2">
-    <h1 class="text-xl font-bold">Boards</h1>
-    <div class="flex flex-row">
-      <Board
-          v-for="board in boards"
-          :key="board.title"
-          :image="board.image"
-          :order="board.order"
-          :tasks="board.tasks"
-          :title="board.title"
-          class="mr-2"
-      ></Board>
-      <kbutton :fill-mode="'flat'" @click="addNewBoard">New Board+</kbutton>
-    </div>
-  </div>
-</template>
-
-<script>
-import Board from '@/components/Board.vue';
-import {Button} from '@progress/kendo-vue-buttons';
-
-export default {
-  name: 'boards',
-  components: {
-    Board,
-    kbutton: Button,
-  },
-  data() {
-    return {
-      boards: [
-        {
-          title: 'My first board',
-          order: '',
-          image: 'https://picsum.photos/285/100',
-          tasks: [],
-        },
-        {
-          title: 'My second board',
-          order: '',
-          image: 'https://picsum.photos/285/100',
-          tasks: [],
-        },
-        {
-          title: 'My third board',
-          order: '',
-          image: 'https://picsum.photos/285/100',
-          tasks: [],
-        },
-      ],
-    };
-  },
-  methods: {
-    addNewBoard() {
-      alert('add a board');
+<script setup lang="ts">
+import { useAlerts } from "@/stores/alerts";
+import type { Board } from "@/types";
+import { ref } from "vue";
+const boards = ref<Partial<Board>[]>([
+  {
+    id: "1",
+    title: "My First Board",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?board=1",
     },
   },
-};
+  {
+    id: "2",
+    title: "My Second Board",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?board=2",
+    },
+  },
+  {
+    id: "3",
+    title: "My Third Board",
+    order: "[]",
+  },
+]);
+
+const alerts = useAlerts();
+
+function createBoard() {
+  alerts.success("Board created!");
+}
 </script>
 
-<style>
-.k-button {
-  width: 285px;
-}
-</style>
+<template>
+  <h1 class="mb-5 text-3xl">Boards</h1>
+  <div class="flex">
+    <BoardCard v-for="board in boards" :key="board.id" :board="board" />
+    <button class="text-gray-500" @click="createBoard()">
+      <span>New Board +</span>
+    </button>
+  </div>
+</template>
