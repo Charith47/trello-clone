543b7121ec7424d1689b6ccae5ea71001898b5f3
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-13 23:20:19 +0530

    import vueuse

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module '@vue/runtime-core' {
     Board: typeof import('./src/components/Board.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TheDrawer: typeof import('./src/components/TheDrawer.vue')['default']
     TheNavbar: typeof import('./src/components/TheNavbar.vue')['default']
   }
 }
