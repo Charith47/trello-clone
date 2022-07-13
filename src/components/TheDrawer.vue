44bf640fb4509378c631ebc1221bd4e7515390c2
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-13 23:22:44 +0530

    use local storage for vue-forge-drawer-expanded

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -3,6 +3,18 @@
 </template>
 
 <script>
+import {useLocalStorage} from "@vueuse/core";
+
+import {computed, ref} from "vue";
+
+// const router = useRouter();
+const selectedId = ref(0);
+
+const expanded = useLocalStorage("vue-forge-drawer-expanded", true);
+const expandedIcon = computed(() =>
+    expanded.value ? "k-i-arrow-chevron-left" : "k-i-arrow-chevron-right"
+);
+
 export default {
   name: "TheDrawer"
 }
